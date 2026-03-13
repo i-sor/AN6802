03cf53951900483f2e919bfb5ff0fc180f656b01
isor <75077364+i-sor@users.noreply.github.com>
2026-03-13 11:45:22 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
+import os
+os.environ["GROQ_API_KEY"] = "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"
 
 model = joblib.load("foodexp.pkl")
 
@@ -34,6 +36,19 @@ def foodexp():
     q = float(request.form.get("q"))
     r = model.predict([[q]])
     return(render_template("foodexp.html",r=r[0][0]))
+
+@app.route("/roe",methods=["get","post"])
+def roe():
+    
+    return(render_template("roe.html"))
+
+@app.route("/generalquestion",methods=["get","post"])
+def generalquestion():
+    return(render_template("generalquestion.html"))
+
+@app.route("/groq",methods=["get","post"])
+def groq():
+    return(render_template("groq.html"))
 
 if __name__ == "__main__":
     app.run()
